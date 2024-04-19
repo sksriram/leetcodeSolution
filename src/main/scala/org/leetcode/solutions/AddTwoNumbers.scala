ca82337650c42495766a9e2c5cecaf7064bcd0f8
Suresh Sriram <sksriram9@gmail.com>
2024-04-19 12:30:51 +0530

    Add two numbers using Linked Lists

---
+++ src/main/scala/org/leetcode/solutions/AddTwoNumbers.scala
@@ -0,0 +1,47 @@
+package org.leetcode.solutions
+
+
+ // Definition for singly-linked list.
+   class ListNode(_x: Int = 0, _next: ListNode = null) {
+     var next: ListNode = _next
+     var x: Int = _x
+  }
+
+
+
+object AddTwoNumbers extends App {
+
+  def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+    // Define a recursive helper function to add two numbers represented as linked lists
+    def addHelper(node1: ListNode, node2: ListNode, carry: Int): ListNode = {
+      if (node1 == null && node2 == null && carry == 0) {
+        null
+      } else {
+        val value1 = if (node1 != null) node1.x else 0
+        val value2 = if (node2 != null) node2.x else 0
+        val sum = value1 + value2 + carry
+        val newNode = new ListNode(sum % 10)
+        // Recursive call to handle the next nodes and carry
+        newNode.next = addHelper(if (node1 != null) node1.next else null, if (node2 != null) node2.next else null, sum / 10)
+        newNode
+      }
+    }
+
+    addHelper(l1, l2, 0)
+  }
+  val list1 = new ListNode(2, new ListNode(4, new ListNode(3)))
+  val list2 = new ListNode(5, new ListNode(6, new ListNode(4)))
+
+
+
+  val addList = addTwoNumbers(list1, list2)
+
+  var ptr = addList
+
+  while (ptr != null)
+  {
+    println(ptr.x)
+    ptr = ptr.next
+  }
+
+}
