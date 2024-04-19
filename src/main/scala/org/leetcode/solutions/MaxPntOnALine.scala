d568f1898913ecfb66502dc5f8859bd6629bfffa
Suresh Sriram <sksriram9@gmail.com>
2024-04-19 14:39:38 +0530

    Max Points on a Line

--- src/main/scala/org/leetcode/solutions/MaxPntOnALine.scala
+++ src/main/scala/org/leetcode/solutions/MaxPntOnALine.scala
@@ -1,19 +1,19 @@
 package org.leetcode.solutions
 
-import scala.collection.mutable.HashMap
+
 import scala.collection.mutable
 
 object MaxPntOnALine extends App {
 
   def maxPoints(points: Array[Array[Int]]): Int = {
 
-    if (points.length <= 2) return points.length // If there are 0, 1, or 2 points, return the number of points
+    if (points.length <= 2) return points.length
 
-    var maxCount = 0 // Initialize the maximum count of points on the same line
+    var maxCount = 0
     for (i <- 0 until points.length - 1) {
-      val slopeCount = mutable.Map[Double, Int]() // Map to store slope and count of points with that slope
-      var overlapping = 0 // Count of points overlapping with the current point
-      var currentMax = 0 // Maximum count of points on the same line passing through the current point
+      val slopeCount = mutable.Map[Double, Int]()
+      var overlapping = 0
+      var currentMax = 0
 
       for (j <- i + 1 until points.length) {
         val x1 = points(i)(0)
@@ -22,14 +22,14 @@ object MaxPntOnALine extends App {
         val y2 = points(j)(1)
 
         if (x1 == x2 && y1 == y2) {
-          overlapping += 1 // Increment overlapping count for points with the same coordinates
+          overlapping += 1
         } else {
           val slope = if (x1 == x2) Double.PositiveInfinity else (y2 - y1).toDouble / (x2 - x1).toDouble
-          slopeCount.put(slope, slopeCount.getOrElse(slope, 0) + 1) // Increment count for the calculated slope
-          currentMax = Math.max(currentMax, slopeCount(slope)) // Update currentMax with the maximum count of points on the same line
+          slopeCount.put(slope, slopeCount.getOrElse(slope, 0) + 1)
+          currentMax = Math.max(currentMax, slopeCount(slope))
         }
       }
-      maxCount = Math.max(maxCount, currentMax + overlapping + 1) // Update maxCount considering overlapping points
+      maxCount = Math.max(maxCount, currentMax + overlapping + 1)
     }
 
     maxCount
